728dced23b56e6c0d7a1306dd550746963d22fdd
hrishitchaudhuri <57035481+hrishitchaudhuri@users.noreply.github.com>
2020-06-15 10:33:19 +0530

    Created makefile

---
+++ library/makefile.mk
@@ -0,0 +1,10 @@
+a.out: example.o linearalgebra.o
+  gcc example.o linearalgebra.o
+  
+  
+example.o: example.c linearalgebra.h
+  gcc example.c linearalgebra.h
+
+
+linearalgebra.o: linearalgebra.c linearalgebra.h
+  gcc linearalgebra.c linearalgebra.h
